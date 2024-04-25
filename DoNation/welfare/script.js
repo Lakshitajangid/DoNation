6e75ce557d7d02f3d19b1ba666eb80de38a4aadd
Lakshita Jangid <134724242+Lakshitajangid@users.noreply.github.com>
2024-04-25 08:51:24 +0530

    Add files via upload

---
+++ DoNation/welfare/script.js
@@ -0,0 +1,76 @@
+
+        function homeless1(){
+            document.getElementById("homeless_slide").src="homeless/slide2.jpg";
+        }
+        function homeless2(){
+            document.getElementById("homeless_slide").src="homeless/slide3.jpg";
+        }
+        function homeless3(){
+            document.getElementById("homeless_slide").src="homeless/slide4.jpg";
+        }
+        function homeless4(){
+            document.getElementById("homeless_slide").src="homeless/slide5.jpg";
+        }
+        function homeless5(){
+            document.getElementById("homeless_slide").src="homeless/slide1.jpg";
+        }
+        setInterval(homeless1,3000);
+        setInterval(homeless2,6000);
+        setInterval(homeless3,9000);
+        setInterval(homeless4,12000);
+        setInterval(homeless5,15000);
+
+
+        
+        function child1(){
+            document.getElementById("child_slide").src="child/smile2.jpg";
+        }
+        function child2(){
+            document.getElementById("child_slide").src="child/smile3.jpg";
+        }
+        function child3(){
+            document.getElementById("child_slide").src="child/smile4.jpg";
+        }
+        function child4(){
+            document.getElementById("child_slide").src="child/smile5.jpg";
+        }
+        function child5(){
+            document.getElementById("child_slide").src="child/smiles.jpg";
+        }
+       
+        setInterval(child1,3000);
+        setInterval(child2,6000);
+        setInterval(child3,9000);
+        setInterval(child4,12000);
+        setInterval(child5,15000);
+        
+
+        function animal1(){
+            document.getElementById("Animal_slide").src="animal/Gcow7.jpg";
+        }
+
+        function animal2(){
+            document.getElementById("Animal_slide").src="animal/Gcat4.jpg";
+        }
+        
+        function animal3(){
+            document.getElementById("Animal_slide").src="animal/Gcow8.jpg";
+        }
+        
+        function animal4(){
+            document.getElementById("Animal_slide").src="animal/Gdog2.jpg";
+        }
+        
+        function animal5(){
+            document.getElementById("Animal_slide").src="animal/Gcat5.jpg";
+        }
+        
+        function animal6(){
+            document.getElementById("Animal_slide").src="animal/Gdog1.jpg";
+        }
+        setInterval(animal1,3000);
+        setInterval(animal2,6000);
+        setInterval(animal3,9000);
+        setInterval(animal4,12000);
+        setInterval(animal5,15000);
+        setInterval(animal6,18000);
